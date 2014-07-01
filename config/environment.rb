f08ede0fb8bcefa911dc9467b94e94bb449366bf
Apprentice <apprentice@dbc31.local>
2014-06-30 22:07:42 -0700

    final env file

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
 require 'redis'
 require 'sidekiq'
+
 # Initialize the rails application
 DbcOverflow::Application.initialize!
 
